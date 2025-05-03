6eb4f68eb7f8e66c21c611a7d8fe6d2df330f8c6
bistasushant <155437951+bistasushant@users.noreply.github.com>
2025-05-03 13:32:37 +0545

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NextTopLoader from "nextjs-toploader";
 import { Toaster } from "sonner";
-import { AuthProvider } from "@/providers/AuthProviders";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,11 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AuthProvider>
-          <NextTopLoader color="#C16AFB" />
-          {children}
-          <Toaster />
-        </AuthProvider>
+        <NextTopLoader color="#C16AFB" />
+        {children}
+        <Toaster />
       </body>
     </html>
   );
